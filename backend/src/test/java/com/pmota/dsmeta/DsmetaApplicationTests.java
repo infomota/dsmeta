e8d2b99a00628d12a344f8b36644af590309d4b5
pmota <paulo_mota25@yahoo.com.br>
2022-11-14 16:12:04 -0300

    Project created

---
+++ backend/src/test/java/com/pmota/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmota.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
