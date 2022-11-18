889804f6fb4fe85449ecc5054bf6a3a50d0461e2
infomota <paulo_mota25@yahoo.com.br>
2022-11-18 17:26:52 -0300

    API test

---
+++ backend/src/main/java/com/pmota/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.pmota.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pmota.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
