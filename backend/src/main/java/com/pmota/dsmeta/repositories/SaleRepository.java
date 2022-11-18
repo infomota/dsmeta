3876a629868d4bc7d381d8615f01c344b8e0ecbb
infomota <paulo_mota25@yahoo.com.br>
2022-11-18 18:51:17 -0300

    Twilio SMS

--- backend/src/main/java/com/pmota/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/pmota/dsmeta/repositories/SaleRepository.java
@@ -12,10 +12,10 @@ import com.pmota.dsmeta.entities.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 		
 	// Query JPQA
-//	String findSales = "SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC";
+	String findSales = "SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC";
 		
 	// Consultas Customizadas 
-	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	@Query(findSales)
 	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
